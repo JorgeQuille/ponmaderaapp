b09ac01984f8ba04c91e662a8d214f1188298896
Jorge Quille <44824514+JorgeQuille@users.noreply.github.com>
2023-02-24 22:40:08 -0500

    first view of the application

--- app/src/main/java/ponmaderaapp/com/MainActivity.java
+++ app/src/main/java/ponmaderaapp/com/MainActivity.java
@@ -3,12 +3,20 @@ package ponmaderaapp.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private WebView webView;
+    private String url = "https://job.ponmadera.com/trabajador";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.visor_web_id);
+        final WebSettings configVisorWeb = webView.getSettings();
+        configVisorWeb.setJavaScriptEnabled(true);
+        webView.loadUrl(url);
     }
 }
