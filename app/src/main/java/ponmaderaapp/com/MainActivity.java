e09a4706b964a82c73cb2e313131d6bc2f19f30a
Jorge Quille <44824514+JorgeQuille@users.noreply.github.com>
2023-02-27 20:55:17 -0500

    check operation of return button

--- app/src/main/java/ponmaderaapp/com/MainActivity.java
+++ app/src/main/java/ponmaderaapp/com/MainActivity.java
@@ -3,8 +3,10 @@ package ponmaderaapp.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
@@ -15,8 +17,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         webView = (WebView) findViewById(R.id.visor_web_id);
-        final WebSettings configVisorWeb = webView.getSettings();
+        WebSettings configVisorWeb = webView.getSettings();
         configVisorWeb.setJavaScriptEnabled(true);
         webView.loadUrl(url);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setAllowFileAccess(true);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
