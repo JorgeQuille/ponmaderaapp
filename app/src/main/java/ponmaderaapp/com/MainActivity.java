8504284e8dd2e4bf9341cd20d65d22b4160c5dc4
Jorge Quille <44824514+JorgeQuille@users.noreply.github.com>
2023-02-27 23:28:15 -0500

    test to app

--- app/src/main/java/ponmaderaapp/com/MainActivity.java
+++ app/src/main/java/ponmaderaapp/com/MainActivity.java
@@ -2,14 +2,18 @@ package ponmaderaapp.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
+    private WebSettings configVisorWeb;
     private String url = "https://job.ponmadera.com/trabajador";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +21,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         webView = (WebView) findViewById(R.id.visor_web_id);
-        WebSettings configVisorWeb = webView.getSettings();
+        configVisorWeb = webView.getSettings();
+        webView.getSettings().setAllowFileAccess(true);
+        configVisorWeb.setDomStorageEnabled(true);
         configVisorWeb.setJavaScriptEnabled(true);
         webView.loadUrl(url);
         webView.setWebViewClient(new WebViewClient());
-        webView.getSettings().setAllowFileAccess(true);
+        webView.setWebChromeClient(new WebChromeClient());
     }
 
+    /*@Override
+    public void onBackPressed()
+    {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }else{
+            super.onBackPressed();
+        }
+    }*/
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
+
         if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
             webView.goBack();
             return true;
